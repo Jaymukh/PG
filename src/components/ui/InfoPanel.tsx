adeb0f492de0cd945194932556c8432de941de9f
ILMA  ALI FAROOQUI <ilma.alifarooqui@tarento.com>
2024-05-13 16:58:36 +0530

    remove unwanted code

--- src/components/ui/InfoPanel.tsx
+++ src/components/ui/InfoPanel.tsx
@@ -13,30 +13,6 @@ const InfoPanel: React.FC<InfoPanelProps> = ({ Icon, text, classname, onClick })
     const [showPopup, setShowPopup] = useState(false);
     const [popupStyle, setPopupStyle] = useState<{ left: number | string; right: number | string; width: any; transform?: string; }>({ left: '', right: '', width: 192 });
 
-    // const handleMouseEnter = () => {
-    //     const popupWidth = 100;
-    //     const infoButtonPosition = menuRef.current?.getBoundingClientRect();
-        
-
-    //     if (infoButtonPosition) {
-    //         const hasSpaceOnLeft = infoButtonPosition.left >= popupWidth / 2;
-    //         const hasSpaceOnRight = window.innerWidth - infoButtonPosition.right >= popupWidth / 2;
-
-    //         if (hasSpaceOnLeft && hasSpaceOnRight) {
-    //             // Enough space on both sides, center the popup
-    //             setPopupStyle({ left: '50%', right: 'auto', width: popupWidth, transform: 'translateX(-50%)' });
-    //         } else if (hasSpaceOnLeft) {
-    //             // Not enough space on the right, position on the left
-    //             const rightPosition = -(window.innerWidth - infoButtonPosition.right + window.scrollX - 10);
-    //             setPopupStyle({ left: 'auto', right: rightPosition, width: popupWidth, transform: 'none' });
-    //         } else if (hasSpaceOnRight) {
-    //             // Not enough space on the left, position on the right
-    //             const lefttPosition = - (infoButtonPosition.left + window.scrollX - 10);
-    //             setPopupStyle({ left: lefttPosition, right: 'auto', width: popupWidth, transform: 'none' });
-    //         }
-    //     }
-    //     setShowPopup(true);
-    // };
     const handleMouseEnter = () => {
         const popupMaxWidth = 192; // Maximum width
         const infoButtonPosition = menuRef.current?.getBoundingClientRect();
